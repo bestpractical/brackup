7f904e35b2ac96c6ef808c9d1e40531ffc487512
bradfitz <bradfitz@c3152786-d43a-0410-8499-09eb43983aed>
2006-09-29 07:00:49 +0000

    make a Brackup.pm, just to hold version number and stuff
    git-svn-id: http://brackup.googlecode.com/svn/trunk@52 c3152786-d43a-0410-8499-09eb43983aed

---
+++ lib/Brackup.pm
@@ -0,0 +1,14 @@
+package Brackup;
+use strict;
+use vars qw($VERSION);
+$VERSION = '0.90';
+
+use Brackup::Config;
+use Brackup::File;
+use Brackup::DigestDatabase;
+use Brackup::Backup;
+use Brackup::Root;     # aka "source"
+use Brackup::Restore;
+use Brackup::Target;
+
+1;
