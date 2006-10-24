d1565ad028a82119d1983f7cc96c847bbf80b07b
mart <mart@c3152786-d43a-0410-8499-09eb43983aed>
2006-10-24 20:26:34 +0000

    The naming scheme for chunk files is rather unfortunate since colons aren't allowed in filenames on Windows.
    My ghetto solution is to add a flag to the config which will turn the colons into dots if set. It's set on automatically on ActiveState Perl on Windows unless explicitly set to off in the configuration file. It can also be explicitly set to on for other systems in case, for some bizarre reason, Brackup is running on a UNIX system but writing its data to an SMB mount from a Windows box.
    
    git-svn-id: http://brackup.googlecode.com/svn/trunk@75 c3152786-d43a-0410-8499-09eb43983aed

--- lib/Brackup/Target/Filesystem.pm
+++ lib/Brackup/Target/Filesystem.pm
@@ -8,6 +8,8 @@ sub new {
     my ($class, $confsec) = @_;
     my $self = bless {}, $class;
     $self->{path} = $confsec->path_value("path");
+    $self->{nocolons} = $confsec->value("no_filename_colons");
+    $self->{nocolons} = ($^O eq 'MSWin32') unless defined $self->{nocolons}; # LAME: Make it work on Windows
     return $self;
 }
 
@@ -34,6 +36,7 @@ sub _diskpath {
     my @parts;
     my $fulldig = $dig;
     $dig =~ s/^\w+://; # remove the "hashtype:" from beginning
+    $fulldig =~ s/:/./g if $self->{nocolons}; # Convert colons to dots if we've been asked to
     while (length $dig && @parts < 4) {
         $dig =~ s/^(.{1,4})//;
         push @parts, $1;
@@ -79,14 +82,18 @@ sub store_chunk {
         File::Path::mkpath($dir) or die "Failed to mkdir: $dir: $!\n";
     }
     open (my $fh, ">$path") or die "Failed to open $path for writing: $!\n";
+    binmode($fh);
     my $chunkref = $chunk->chunkref;
     print $fh $$chunkref;
     close($fh) or die "Failed to close $path\n";
 
     my $actual_size   = -s $path;
     my $expected_size = length $$chunkref;
+    unless (defined($actual_size)) {
+        die "Chunk output file $path does not exist. Do you need to set no_filename_colons=1?";
+    }
     unless ($actual_size == $expected_size) {
-        die "Chunk was written to disk wrong:  size is $actual_size, expecting $expected_size\n";
+        die "Chunk $path was written to disk wrong:  size is $actual_size, expecting $expected_size\n";
     }
 
     if (my $mstr = $chunk->meta_contents) {
