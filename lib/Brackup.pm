f75063db595f898c27b3cf0fef6394989e6baa19
bradfitz <bradfitz@c3152786-d43a-0410-8499-09eb43983aed>
2007-05-23 02:00:38 +0000

    Checking in changes prior to tagging of version 1.02.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(revision 135)
    +++ Changes	(working copy)
    @@ -1,3 +1,5 @@
    +1.02 (may 22, 2007)
    +
       - support for merging little files together into big chunks
         on the backup target.  aka "tail packing".  requires no changes
         to target drivers.  this should speed backups, as less network
    
    
    git-svn-id: http://brackup.googlecode.com/svn/trunk@136 c3152786-d43a-0410-8499-09eb43983aed

--- lib/Brackup.pm
+++ lib/Brackup.pm
@@ -1,7 +1,7 @@
 package Brackup;
 use strict;
 use vars qw($VERSION);
-$VERSION = '1.01';
+$VERSION = '1.02';
 
 use Brackup::Config;
 use Brackup::ConfigSection;
---
+++ makedocs.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $base = "/home/lj/htdocs/dev/brackup/";
+my $pshb = Goats->new;
+$pshb->batch_convert([qw(brackup brackup-restore lib)], $base);
+
+package Goats;
+
+use strict;
+use base 'Pod::Simple::HTMLBatch';
+
+sub modnames2paths {
+    my ($self, $dirs) = @_;
+
+    my @files;
+    my @dirs;
+
+    foreach my $path (@{$dirs || []}) {
+        if (-f $path) {
+            push @files, $path;
+        } else {
+            push @dirs, $path;
+        }
+    }
+
+    my $m2p = $self->SUPER::modnames2paths(\@dirs);
+
+    foreach my $file (@files) {
+        my ($tail) = $file =~ m!([^/]+)\z!;
+        $m2p->{$tail} = $file;
+    }
+
+    # these are symlinks in brad's lib
+    foreach my $k (keys %$m2p) {
+        delete $m2p->{$k} if $k eq "Danga::blib::lib::Danga::Socket" || $k eq "Danga::Socket";
+    }
+
+    return $m2p;
+}
