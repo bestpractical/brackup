fb8909957db74605ec23101971b892e42851bd62
brad@danga.com <brad@danga.com@c3152786-d43a-0410-8499-09eb43983aed>
2007-10-16 03:10:40 +0000

    - new on-disk layout for the Filesystem target.  fully backwards     compatible with the old layout.  also, if max-link count (max     files in a directory) is hit, this new version will carefully     rearrange the minimum files necessary to the new layout to     make room, all automatically.  the new format is xx/xx/*,     rather than xxxx/xxxx/xxxx/xxxx/* which was stupid and overkill.     stupid because that's 65k of files in the root, twice ext3's     limit, and overkill because leaves were always just 1 file.     thanks to Max Kanat-Alexander for pointing this out, and     part of the patch to use new layout pattern.
    git-svn-id: http://brackup.googlecode.com/svn/trunk@168 c3152786-d43a-0410-8499-09eb43983aed

--- lib/Brackup/Target/Filesystem.pm
+++ lib/Brackup/Target/Filesystem.pm
@@ -14,6 +14,11 @@ sub new {
     $self->{path} = $confsec->path_value("path");
     $self->{nocolons} = $confsec->value("no_filename_colons");
     $self->{nocolons} = ($^O eq 'MSWin32') unless defined $self->{nocolons}; # LAME: Make it work on Windows
+
+    if ($ENV{BRACKUP_REARRANGE_FS_TARGET}) {
+        $self->_upgrade_layout;
+    }
+
     return $self;
 }
 
@@ -35,22 +40,170 @@ sub backup_header {
     };
 }
 
-sub _diskpath {
-    my ($self, $dig, $ext) = @_;
+# 1.05 and before stored files on disk as: xxxx/xxxx/xxxxxxxxxx.brackup
+# (that is, two levels of directories, each 4 hex digits long, or 65536
+# files per directory, which is 2x what ext3 can store, leading to errors.
+# in 1.06 and above, xx/xx/xxxxxx is used.  that is, two levels of 2 hex
+# digits.  this function 
+sub _upgrade_layout {
+    my $self = shift;
+    my $clean_limit = shift; # optional; if set, max top-level dirs to clean
+
+    my $root = $self->{path};
+
+    opendir(my $dh, $root) or die "Error opening $root: $!";
+
+    # read the current state of things in the root directory
+    # (which is presumably maxed out on files, at 32k or whatnot)
+    my %exist_twodir;    # two_dir -> 1 (which two-letter directories exist)
+    my %exist_fourdir;   # four_dir -> 1 (which four-letter directories exist)
+    my %four_of_two;     # two_dir -> [ four_dir, four_dir, ... ]
+    while (my $dir = readdir($dh)) {
+        next unless -d "$root/$dir";
+        if ($dir =~ /^[0-9a-f]{2}$/) {
+            $exist_twodir{$dir} = 1;
+            next;
+        }
+        if ($dir =~ /^([0-9a-f]{2})([0-9a-f]{2})$/) {
+            $exist_fourdir{"$1$2"} = 1;
+            push @{ $four_of_two{$1} ||= [] }, "$1$2";
+        }
+    }
+
+    # for each 4-digit directory, sorted by number of four-digit directories
+    # that exist for their leading 2-digit prefix (to most quickly free up
+    # a link in root, in 2 iterations), 
+    # see if the "01/" directory exists (the leading two bytes).
+    # if not,
+    #    move it to some random other 'xxxx' directory,
+    #    as, say, "abcd/tmp-was-root-0123".
+    # now, for either the "0123" directory or "tmp-was-root-0123"
+    # directory, file all chunks, and move them to the
+    # right locations "01/23/*.chunk", making "01/23" if needed.
+    # (shouldn't be any out-of-link problems down one level)
+    my @four_dirs = map {
+        sort @{ $four_of_two{$_} }
+    }
+    sort {
+        scalar(@{ $four_of_two{$b} }) <=> scalar(@{ $four_of_two{$a} })
+    } keys %four_of_two;
+    my $n_done;
+    while (my $four_dir = shift @four_dirs) {
+        my $leading_two = substr($four_dir, 0, 2);
+        my $migrate_source;
+        if ($exist_twodir{$leading_two}) {
+            # top-level destination already exists.  no need for more
+            # links in the top-level
+            $migrate_source = $four_dir;
+        } elsif (@four_dirs) {
+            # we need to move four_dir away, into another four_dir,
+            # to make room to create a new two_dir in the root
+            my $holder_four_dir = $four_dirs[0];
+            $migrate_source = "$holder_four_dir/tmp-was-root-$four_dir";
+            my $temp_dir = "$root/$migrate_source";
+            rename "$root/$four_dir", $temp_dir
+                or die "Rename of $root/$four_dir -> $temp_dir failed: $!";
+        } else {
+            # no four_dirs left?  then I bet we aren't out of links
+            # anymore.  just migrate.
+            $migrate_source = $four_dir;
+        }
+
+        $self->_upgrade_chunks_in_directory($four_dir, $migrate_source);
+        if (-e "$root/$four_dir") {
+            die "Upgrade of $root/$four_dir/* didn't seem to have worked.";
+        }
+        $n_done++;
+        last if $clean_limit && $n_done >= $clean_limit;
+    }
+}
+
+sub _upgrade_chunks_in_directory {
+    my $self = shift;
+    my $four_dig = shift;  # first four hex digits of all files being moved
+    my $rel_dir = shift;   # directory (relative to root) to move files from, and then remove
+    die "not relative" unless $rel_dir =~ m!^[^/]!;
+
+    my $root = $self->{path};
+
+    my ($hex12, $hex34) = $four_dig =~ /^([0-9a-f]{2})([0-9a-f]{2})$/
+        or die "four_dig not four hex digits";
+
+    my $dest_dir0 = "$root/$hex12";
+    my $dest_dir  = "$root/$hex12/$hex34";
+    for ($dest_dir0, $dest_dir) {
+        next if -d $_;
+        mkdir $_ or die "Failed to mkdir $_: $!";
+    }
+
+    my @dirs;
+    File::Find::find({wanted => sub {
+        my $name = $File::Find::name;
+        if (-f $name) {
+            my $basefile = $_;  # stupid File::Find conventions
+            rename $name, "$dest_dir/$basefile" or die
+                "Failed to move $name to $dest_dir: $!";
+        } elsif (-d $name) {
+            return if $_ eq "." || $_ eq "..";
+            push @dirs, $name;
+        }
+    }}, "$root/$rel_dir");
+
+    my $final_dir = "$root/$four_dig";
+    for my $dir (reverse(@dirs), $final_dir) {
+        if (!rmdir($dir) && -d $dir) {
+            warn "Directory not empty? $dir.  Skipping cleanup.\n";
+            return;
+        }
+    }
+    warn "Rearranged & removed $four_dig\n";
+}
+
+# version <= 1.05: 0123/4567/89ab/cdef/0123456789abcdef...xxx.chunk
+# this is totally stupid.  65k files in root (twice ext3's historical/common
+# maximum), and the leaves were always containing but one file.
+sub _old_diskpath {
+    my ($self, $dig) = @_;
     my @parts;
     my $fulldig = $dig;
     $dig =~ s/^\w+://; # remove the "hashtype:" from beginning
     $fulldig =~ s/:/./g if $self->{nocolons}; # Convert colons to dots if we've been asked to
     while (length $dig && @parts < 4) {
-        $dig =~ s/^(.{1,4})//;
+        $dig =~ s/^([0-9a-f]{4})// or die "Can't get 4 hex digits of $fulldig";
         push @parts, $1;
     }
-    return $self->{path} . "/" . join("/", @parts) . "/$fulldig.$ext";
+    return $self->{path} . "/" . join("/", @parts) . "/$fulldig.chunk";
+}
+
+# version >= 1.06: 01/23/0123456789abcdef...xxx.chunk
+# 256 * 256 directories, then files.  would need 2 billion
+# files before leaves have 32k+ files, but at that point
+# users are probably using better filesystems if they
+# have 2+ inodes.
+sub _new_diskpath {
+    my ($self, $dig) = @_;
+    my @parts;
+    my $fulldig = $dig;
+    $dig =~ s/^\w+://; # remove the "hashtype:" from beginning
+    $fulldig =~ s/:/./g if $self->{nocolons}; # Convert colons to dots if we've been asked to
+    while (length $dig && @parts < 2) {
+        $dig =~ s/^([0-9a-f]{2})// or die "Can't get 2 hex digits of $fulldig";
+        push @parts, $1;
+    }
+    return $self->{path} . "/" . join("/", @parts) . "/$fulldig.chunk";
 }
 
 sub chunkpath {
     my ($self, $dig) = @_;
-    return $self->_diskpath($dig, "chunk");
+
+    # if the old (version <= 1.05) chunk still exists,
+    # just use that.
+    my $old = $self->_old_diskpath($dig);
+    return $old if -e $old;
+
+    # else, use the new (version >= 1.06) location, which
+    # is much more sensible
+    return $self->_new_diskpath($dig);
 }
 
 sub has_chunk_of_handle {
@@ -77,7 +230,23 @@ sub store_chunk {
     my $dir = $path;
     $dir =~ s!/[^/]+$!!;
     unless (-d $dir) {
-        File::Path::mkpath($dir) or die "Failed to mkdir: $dir: $!\n";
+        unless (eval { File::Path::mkpath($dir) }) {
+            if ($!{EMLINK}) {
+                warn "Too many directories in one directory; doing partial cleanup before proceeding...\n";
+                # NOTE: 2 directories is key to freeing up one link.  imagine upgrading one:
+                # it'd remove "0000" but possibly (likely) create "00".  so we do two,
+                # because, following the example, "0001" would also go into "00", so we'd have one
+                # link left in the root.  _upgrade_layout orders the directories to clean in
+                # an order such that 2 will succeed or fail, but no higher will succeed when
+                # 2 won't.
+                $self->_upgrade_layout(2);
+                unless (eval { File::Path::mkpath($dir) }) {
+                    die "Still can't create directory $dir: $!\n";
+                }
+            } else {
+                die "Failed to mkdir: $dir: $!\n";
+            }
+        }
     }
     open (my $fh, ">$path") or die "Failed to open $path for writing: $!\n";
     binmode($fh);
